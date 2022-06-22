797bca31244ce430a22330bb364d1a0a736bbeaa
moaz886 <56744153+moaz886@users.noreply.github.com>
2022-06-22 04:30:15 +0200

    Stage 2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,14 +5,13 @@ import org.scalatest.funsuite.AnyFunSuite
 class coffeeShopTesting extends AnyFunSuite{
   var cafe = new coffeeShop;
 
-
   test("Test 1") {
     var bill: List[String] =List("Cola", "Coffee", "Cheese Sandwich")
-    assert(cafe.totalBill(bill) === 3.5)
+    assert(cafe.totalBill(bill) === 3.85)
   }
   test("Test 2") {
     var bill: List[String] =List("Cola", "Steak Sandwich", "Cheese Sandwich" , "Coffee")
-    assert(cafe.totalBill(bill) === 8.0)
+    assert(cafe.totalBill(bill) === 9.6)
   }
   test("Test 3") {
     var bill: List[String] =List("Cola")
@@ -21,23 +20,47 @@ class coffeeShopTesting extends AnyFunSuite{
 
   test("Test 4") {
     var bill: List[String] =List("Cheese Sandwich" ,"Cheese Sandwich")
-    assert(cafe.totalBill(bill) === 4.0)
+    assert(cafe.totalBill(bill) === 4.4)
+  }
+
+  test("Test 5") {
+    var bill: List[String] =List("Steak Sandwich" ,"Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich")
+    assert(cafe.totalBill(bill) === 32.4)
   }
 }
 
 class coffeeShop{
 
-  val menu = Map("Cola" -> ("Cold",0.50),
-    "Coffee"-> ("Hot",1.0),
-    "Cheese Sandwich"-> ("Cold",2.0),
-    "Steak Sandwich"-> ("Hot",4.50))
+  val menu = Map("Cola" -> ("Cold",0.50,"Drink"),
+    "Coffee"-> ("Hot",1.0,"Drink"),
+    "Cheese Sandwich"-> ("Cold",2.0,"Food"),
+    "Steak Sandwich"-> ("Hot",4.50,"Food"))
 
   def totalBill(items : List[String]  ) : Double ={
     var billSum=0.0
+    var serviceCharge=0.0
+    var hasFood=false
+    var hasHotFood=false
 
-    if(items.nonEmpty)
-      items.foreach(billSum +=menu(_)._2)
-    println(billSum)
+    for(item <- items){
+      if(menu(item)._3.compareTo("Food") == 0 && menu(item)._1.compareTo("Hot") == 0)
+        hasHotFood=true;
+      else if(menu(item)._3.compareTo("Food") ==0)
+        hasFood=true;
+
+      billSum +=menu(item)._2
+    }
+    if(hasHotFood)
+      serviceCharge=billSum*0.20;
+    else if(hasFood)
+      serviceCharge=billSum*0.10;
+
+    serviceCharge =Math.min(serviceCharge,20.0);
+    billSum+=serviceCharge
+
+    // round to 2 decimal places
+    billSum= (Math.round(billSum*100)/100.0)
+
     billSum
   }
 
