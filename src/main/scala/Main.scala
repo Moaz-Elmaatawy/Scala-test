f3a270273230a2b4b1d5d1f644a1d137a7cc37a9
moaz886 <moaznabil886@gmail.com>
2022-06-28 01:03:31 +0200

    Functional programming edit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,26 +6,26 @@ class coffeeShopTesting extends AnyFunSuite{
   var cafe = new coffeeShop;
 
   test("Test 1") {
-    var bill: List[String] =List("Cola", "Coffee", "Cheese Sandwich")
-    assert(cafe.totalBill(bill) === 3.85)
+    val bill: List[String] =List("Cola", "Coffee", "Cheese Sandwich")
+    assert(cafe.functionalTotalBill(bill) === 3.85)
   }
   test("Test 2") {
-    var bill: List[String] =List("Cola", "Steak Sandwich", "Cheese Sandwich" , "Coffee")
-    assert(cafe.totalBill(bill) === 9.6)
+    val bill: List[String] =List("Cola", "Steak Sandwich", "Cheese Sandwich" , "Coffee")
+    assert(cafe.functionalTotalBill(bill) === 9.6)
   }
   test("Test 3") {
-    var bill: List[String] =List("Cola")
-    assert(cafe.totalBill(bill) === 0.5)
+    val bill: List[String] =List("Cola")
+    assert(cafe.functionalTotalBill(bill) === 0.5)
   }
 
   test("Test 4") {
-    var bill: List[String] =List("Cheese Sandwich" ,"Cheese Sandwich")
-    assert(cafe.totalBill(bill) === 4.4)
+    val bill: List[String] =List("Cheese Sandwich" ,"Cheese Sandwich")
+    assert(cafe.functionalTotalBill(bill) === 4.4)
   }
 
   test("Test 5") {
-    var bill: List[String] =List("Steak Sandwich" ,"Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich")
-    assert(cafe.totalBill(bill) === 32.4)
+    val bill: List[String] =List("Steak Sandwich" ,"Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich")
+    assert(cafe.functionalTotalBill(bill) === 32.4)
   }
 }
 
@@ -36,32 +36,20 @@ class coffeeShop{
     "Cheese Sandwich"-> ("Cold",2.0,"Food"),
     "Steak Sandwich"-> ("Hot",4.50,"Food"))
 
-  def totalBill(items : List[String]  ) : Double ={
-    var billSum=0.0
-    var serviceCharge=0.0
-    var hasFood=false
-    var hasHotFood=false
 
-    for(item <- items){
-      if(menu(item)._3.compareTo("Food") == 0 && menu(item)._1.compareTo("Hot") == 0)
-        hasHotFood=true;
-      else if(menu(item)._3.compareTo("Food") ==0)
-        hasFood=true;
+  def functionalTotalBill(items : List[String]  ) : Double ={
+    val billSum =items.map(S => menu(S)._2).sum
 
-      billSum +=menu(item)._2
-    }
-    if(hasHotFood)
-      serviceCharge=billSum*0.20;
-    else if(hasFood)
-      serviceCharge=billSum*0.10;
+    val hasFood=items.map(S =>  menu(S)._3).contains("Food")
 
-    serviceCharge =Math.min(serviceCharge,20.0);
-    billSum+=serviceCharge
+    val hasHotFood=items.map(S => (menu(S)._1, menu(S)._3)).contains(("Hot", "Food"))
+
+    val serviceCharge = if (hasHotFood) Math.min(0.2*billSum,20.0) else if (hasFood) Math.min(0.1*billSum,20.0) else 0
 
     // round to 2 decimal places
-    billSum= (Math.round(billSum*100)/100.0)
+    val TotalBillSum= (Math.round((billSum+serviceCharge)*100)/100.0)
 
-    billSum
+    TotalBillSum
   }
 
 }
